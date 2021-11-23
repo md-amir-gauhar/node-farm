c84bf93d0b99d3c69319162349c170b06faaa1ad
md-amir-gauhar <gauharamir15@gmail.com>
2021-11-23 13:50:10 +0530

    implemented Routing

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-const fs = require('fs');
 const http = require('http');
-
-///////////////////////////////////////
-// Files
-
-// Blocking, Synchronus way
-// const text = fs.readFileSync('./txt/input.txt', 'utf-8');
-
-// console.log(text);
-
-// const outText = `This is output text : ${text}.\nCreated on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', outText)
-
-// Non-blocking, asynchronus way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log("EROORRR! 💥");
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
-//       console.log('WRITINGGG!!!');
-//     })
-//   })
-// })
-
-
-///////////////////////////////////////
-// Server
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end("This is the Overview Page");
+  } else if (pathName === '/product') {
+    res.end("This is the Product page")
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end('<h1>Page not found!</h1>')
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
