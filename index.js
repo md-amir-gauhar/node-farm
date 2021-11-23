4c49ba6dcecb81b448323c979ba476c4328222cd
md-amir-gauhar <gauharamir15@gmail.com>
2021-11-23 22:44:38 +0530

    added html templates

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+const tempOverview = fs.readFileSync(`${__dirname}/template/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/template/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview Page
   if (pathName === '/' || pathName === '/overview') {
-    res.end("This is the Overview Page");
+
+
+
+    res.end(tempOverview);
+
+    // Product Page
   } else if (pathName === '/product') {
     res.end("This is the Product page")
+
+    //API
   } else if (pathName === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json'
     });
     res.end(data);
 
+    // Not Found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
